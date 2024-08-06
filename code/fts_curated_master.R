ff0e0cd6d8631065b8cd9bd5eab450444ffc4e55
akmiller01 <alex.k.miller@gmail.com>
2024-08-06 14:14:51 -0400

    Slash for path

--- code/fts_curated_master.R
+++ code/fts_curated_master.R
@@ -1,7 +1,7 @@
 fts_save_master <- function(years = 2000:2023, update_years = years, path = "IHA/datasets/"){
   fts_all <- fts_curated_flows(years, update_years = update_years, dataset_path = path)
   for(i in 1:length(years)){
-    fwrite(fts_all[year == years[[i]]], paste0(path, "fts_curated_", years[[i]], ".csv"))
+    fwrite(fts_all[year == years[[i]]], paste0(path, "/fts_curated_", years[[i]], ".csv"))
   }
 }
 
